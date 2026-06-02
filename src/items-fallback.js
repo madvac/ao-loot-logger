c0503aa9a0d9cfc0614910a3b65c4d7d9e2e190f
madvac <30960973+madvac@users.noreply.github.com>
2026-06-02 12:06:09 +0200

    fix: Fix formatting in items-fallback.js

--- src/items-fallback.js
+++ src/items-fallback.js
@@ -1,4 +1,4 @@
-module.exports = `            1: UNIQUE_HIDEOUT                                                   : Hideout Construction Kit
+module.exports = `   1: UNIQUE_HIDEOUT                                                   : Hideout Construction Kit
    2: T3_2H_TOOL_TRACKING                                              : Journeyman's Tracking Toolkit
    3: T4_2H_TOOL_TRACKING                                              : Adept's Tracking Toolkit
    4: T5_2H_TOOL_TRACKING                                              : Expert's Tracking Toolkit
